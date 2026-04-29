23f6364a7dccc715390046d7413830947eb0744b
hotel <therasa.a2005@gmail.com>
2026-04-29 13:46:00 +0530

    muttonbiriyani details

--- hotel.js
+++ hotel.js
@@ -3,15 +3,18 @@ console.log("Hotel Menu");
 console.log("Chicken Biryani - 150");
 console.log("Parotta - 20");
 console.log("Fried Rice - 120");
+console.log("Mutton Biryani - 250");
 
 console.log("You ordered Chicken Biryani");
 console.log("You ordered Parotta");
 console.log("You ordered Fried Rice");
+console.log("You ordered Mutton Biryani");
 
 console.log("Total = 290");
 
 console.log("friedrice is tasty");
 console.log("parotta is so soft");
 console.log("chicken 65 is very yummy");
+console.log("mutton biryani is very delicious");
 
 console.log("Sambar rice includes vegetables");
